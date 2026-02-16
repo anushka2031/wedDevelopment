3590eab84844ccbb629bb7346198d6ea0a950a67
anushka2031 <anushkatiwari2031@gmail.com>
2026-02-16 12:57:16 +0530

    Protfolio

---
+++ Protfolio/prot.js
@@ -0,0 +1,65 @@
+function scrollIntro(){
+
+    const element = document.getElementById("intro");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+
+}
+
+function scrollServices(){
+
+    const element = document.getElementById("content");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    
+}
+
+function scrollExp(){
+
+    const element = document.getElementById("exp");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    
+}
+
+function scrollGoals(){
+
+    const element = document.getElementById("DC");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    
+}
+
+function scrollProjects(){
+
+    const element = document.getElementById("heading");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    
+}
+
+function scrollContact(){
+
+    const element = document.getElementById("contact");
+
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    
+}
