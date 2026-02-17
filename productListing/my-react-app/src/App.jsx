38d3fa2e49b797ab281e98ee08a5bb7133ea9fbe
anushka2031 <anushkatiwari2031@gmail.com>
2026-02-17 22:44:51 +0530

    17 Feb

--- productListing/my-react-app/src/App.jsx
+++ productListing/my-react-app/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
    
 
   let [apiData, SetData] = useState([]);
+  const [filterData , setFilteredData] = useState([]);
+  const [search , setSearch] = useState("");
+  const [sorted , setSorted] = useState("")
  
   useEffect(()=>{
     fetch("https://dummyjson.com/products").then((res) => {
@@ -14,33 +17,75 @@ const App = () => {
     })
     .then((data) =>{
       console.log(data);
+
       
       SetData(data.products)
+      setFilteredData(data.products)
+      
     })
   },[])
 
+  const handleSearch = () =>{
+    if(!search.trim()) {
+      alert("Search feild is empty!")
+      return;
+    }
+
+    const filtered = apiData.filter((item) =>
+      item.title.toLowerCase().includes(search.toLowerCase())
+    )
+    // console.log(filterData);
+    
+    setFilteredData(filtered)
+  }
+
+  const handleLtoH = () =>{
+    const sort = apiData.sort((a,b) => a.price - b.price)
+    setSorted(sort)
+
+  } 
+
+  const handleHtoL = () =>{
+    const sort = apiData.sort((a,b) => b.price - a.price)
+    // console.log(sorted);
+    
+    setSorted(sort)
+
+  } 
+
+
+
+  
+
   return (
     <div id='container'>
-      {/* <input type='text' name='search'/> */}
-      {/* <input type="submit" /> */}
-      {apiData.map((res) =>{
+      <button onClick={() => handleLtoH()}>low to high</button>
+      <button onClick={() => handleHtoL()}>High to low</button>
+           
+      <input type='text' name='search'   onChange={(e)=>setSearch(e.target.value)}/> 
+       <button onClick={handleSearch}>Search</button> 
+       
+      {/* {apiData.map((res) =>{
         return <div id='products'>
                   <h1>{res.title}</h1>
                   <img src={res.images[0]} alt="" />
                   <h2>{res.name}</h2>
                   <h3>{res.price}</h3>
               </div>
-      })}
+      })} */}
 
-      {/* {
-        apiData.filter((res) =>{
-          res.price > 600
-          
+      {
+        filterData.map((res) =>{
+          return <div id='products'>
+                  <h1>{res.title}</h1>
+                  <img src={res.images[0]} alt="" />
+                  <h2>{res.name}</h2>
+                  <h3>{res.price}</h3>
+              </div>
         })
-        // .sort((a,b) =>{
-            
-        //   })
-      } */}
+      }
+
+      
    
     </div>
   )
